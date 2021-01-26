86591866112ca1402d8228000ebda0bf123ca837
jenny <jenny.huaman@gmail.com>
2021-01-25 22:47:57 -0500

    class brower

--- SeleniumFramework/src/test/java/testjava/browserTest.java
+++ SeleniumFramework/src/test/java/testjava/browserTest.java
@@ -1,7 +1,6 @@
 package testjava;
 
 import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,16 +10,15 @@ public class browserTest {
 	
 	public static void main(String[] args) throws Exception {
 		
-		// location to the project
+		 //location to the project
 //		String projectPath=System.getProperty("user.dir");
 //		System.out.println("project path" + projectPath);
 
-		System.setProperty("webdriver.chrome.driver", "D:\\JavaProjects\\SeleniumFramework\\lib\\drivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\jenny\\git\\TestProject\\SeleniumFramework\\lib\\drivers\\chromedriver.exe");
 		//WebDriver driver1=new FirefoxDriver();
 		WebDriver driver = new ChromeDriver();
 		//WebDriver driver=new InternetExplorerDriver();
 		driver.get("https://google.com/");
-		
 		driver.findElement(By.xpath("//input[@class=\"gLFyf gsfi\"]")).sendKeys("automation step by step");
 		
 		List<WebElement> listOfInputElements = driver.findElements(By.xpath("//input"));
@@ -28,7 +26,6 @@ public class browserTest {
 		System.out.println("Count of elements:  "+listOfInputElements.size());
 		
 	    Thread.sleep(3000);
-//
 		driver.close();
 	    driver.quit();		
 	    
